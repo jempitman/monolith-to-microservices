83b73747103b94ae90a9cad4af2cf44c6abe2166
jempitman <80392341+jempitman@users.noreply.github.com>
2022-05-10 15:36:07 +0200

    Fix environment variable bug

--- lesson-4-automating-development/exercises/travis_env_var_exercise/server.js
+++ lesson-4-automating-development/exercises/travis_env_var_exercise/server.js
@@ -7,10 +7,26 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("${FAVORITE_INSTRUCTOR} rocks!");
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
 }
 
 main();
+
+// // Set a local variable populated by the environment variable $NAME
+// const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function main() {
+//   while(true) {
+//     console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
+//     await sleep(5000);
+//   }
+// }
+
+// main();
